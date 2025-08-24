39919f74ff8ec4375feda6e2343674f0a0ce49c0
mmujtabamain <226957529+mmujtabamain@users.noreply.github.com>
2025-08-24 23:20:35 +0500

    Update `main.cpp` message.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include <fmt/core.h>
 
 int main() {
-    fmt::print("Hello from your C++ template!\n");
-
-    int answer = 42;
-    fmt::print("The answer is: {}\n", answer);
+    fmt::print("Hello from C++ template!\n");
 
     return 0;
 }
